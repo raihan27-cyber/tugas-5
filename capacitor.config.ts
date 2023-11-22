0e4e00ae5ea11185e9d84f2c6d8cf5ade4901f6c
delana <raihandelana@gmail.com>
2023-11-22 09:58:41 +0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'apkbelajar',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
